19fce8a0d1e0f248b978dabf214743b05489a700
Ishika Gupta <68727405+ishika-gupta178@users.noreply.github.com>
2024-11-04 16:23:14 -0700

    Update historical_bidding_trends_viz.py
    Deleted the lines related to subset columns

--- historical_bidding_trends_viz.py
+++ historical_bidding_trends_viz.py
@@ -14,10 +14,6 @@ for i in range(1,11):
     price_cols.append(f'QSE submitted Curve-Price{i}')
     supply_cols.append(f'QSE submitted Curve-MW{i}')
 
-subset_cols = []
-subset_cols.extend(price_cols)
-subset_cols.extend(supply_cols)
-df = df[subset_cols]
 
 # Create the first graph (Graph 1)
 def create_graph1(selected_date, selected_unit, selected_resource_type):
